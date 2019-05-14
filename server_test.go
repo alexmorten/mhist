cd965586e0a9fc4783347ef610cfc064e31adef5
alexmorten <alexandrius.martinus@gmail.com>
2019-05-14 10:12:12 +0200

    add test for start param

--- server_test.go
+++ server_test.go
@@ -79,3 +79,71 @@ func Test_Server(t *testing.T) {
 
 	})
 }
+
+func Test_ServerParams(t *testing.T) {
+	formerDataPath := dataPath
+	dataPath = "test_data"
+	defer func() {
+		os.RemoveAll(dataPath)
+		dataPath = formerDataPath
+	}()
+	server := NewServer(ServerConfig{MemorySize: 24 * 1024 * 1024, DiskSize: 24 * 1024 * 1024})
+
+	Convey("HTTPHandler", t, func() {
+		Convey("POSTing and GETing measurements works", func() {
+			numericalValues := []float64{60, 10, 40, 20, 50, 42}
+			for i, value := range numericalValues {
+				w := httptest.NewRecorder()
+				body := fmt.Sprintf(`{"name":"some_name","value":%v, "timestamp":%v}`, value, i*1000+1000)
+				reader := bytes.NewReader([]byte(body))
+				req, _ := http.NewRequest("POST", "/", reader)
+
+				server.httpHandler.ServeHTTP(w, req)
+				So(w.Code, ShouldEqual, 200)
+			}
+
+			categoricalValues := []string{"a", "b", "a", "de", "c", "b", "a"}
+			for i, value := range categoricalValues {
+				w := httptest.NewRecorder()
+				body := fmt.Sprintf(`{"name":"some_other_name","value":"%v", "timestamp":%v}`, value, i*1000)
+				reader := bytes.NewReader([]byte(body))
+				req, _ := http.NewRequest("POST", "/", reader)
+
+				server.httpHandler.ServeHTTP(w, req)
+				So(w.Code, ShouldEqual, 200)
+			}
+
+			// commit diskStore to avoid sleeping
+			server.store.diskStore.commit()
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/?start=2001", nil)
+			server.httpHandler.ServeHTTP(w, req)
+			So(w.Code, ShouldEqual, 200)
+
+			response := map[string][]map[string]interface{}{}
+			body, err := ioutil.ReadAll(w.Body)
+			fmt.Println(string(body))
+			So(err, ShouldBeNil)
+			err = json.Unmarshal(body, &response)
+			So(err, ShouldBeNil)
+			So(len(response["some_name"]), ShouldEqual, len(numericalValues)-2)
+			So(len(response["some_other_name"]), ShouldEqual, len(categoricalValues)-2)
+
+			numericalResponseValues := []float64{}
+			for _, measurement := range response["some_name"] {
+				numericalResponseValues = append(numericalResponseValues, measurement["value"].(float64))
+			}
+
+			So(numericalResponseValues, ShouldResemble, numericalValues[2:])
+
+			categoricalResponseValues := []string{}
+			for _, measurement := range response["some_other_name"] {
+				categoricalResponseValues = append(categoricalResponseValues, measurement["value"].(string))
+			}
+
+			So(categoricalResponseValues, ShouldResemble, categoricalValues[2:])
+		})
+
+	})
+}
